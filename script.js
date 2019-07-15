64d5bb943093cfdb6e494908b7fa91da31622dae
Huiqin <kathryn.h.gao@gmail.com>
2019-07-15 15:05:31 +0800

    Complete task #18

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Images behind doors
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let beachDoorPath = "ttps://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
 // Door #1
 let doorImage1 = document.getElementById('door1');
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 };

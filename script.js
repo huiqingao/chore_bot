8052e064e0d11943a59ae9d85785e5d0a37b9705
Huiqin <kathryn.h.gao@gmail.com>
2019-07-15 15:02:01 +0800

    Complete task #15

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+// Door #1
 let doorImage1 = document.getElementById('door1');
 let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 };
+
+// Door #2
+let doorImage2 = document.getElementById('door2');
+
+// Door #3
+let doorImage3 = document.getElementById('door3');
